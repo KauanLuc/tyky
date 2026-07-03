d80570a5082b264b70c9f1dfb333c0b65a58f503
KauanLuc <kauanfigueiredolucena2016@gmail.com>
2026-07-03 20:22:13 -0300

    feat: Spring Boot initial setup

---
+++ src/main/java/com/flashcards/tyky/TykyApplication.java
@@ -0,0 +1,12 @@
+package com.flashcards.tyky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TykyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TykyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashcards/tyky/TykyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcards.tyky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TykyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
